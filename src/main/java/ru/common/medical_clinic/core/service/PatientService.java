e6d2941aa5267040bee985b9596d7c54da7611bc
Py3aHa <Py3aHa99@gmail.com>
2022-02-27 18:29:44 +0600

    Implementation of CRUD entities of patients and medical histories

---
+++ src/main/java/ru/common/medical_clinic/core/service/PatientService.java
@@ -0,0 +1,8 @@
+package ru.common.medical_clinic.core.service;
+
+
+import ru.common.medical_clinic.core.model.entity.PatientEntity;
+
+public interface PatientService extends BasicCRUDService<PatientEntity> {
+
+}
